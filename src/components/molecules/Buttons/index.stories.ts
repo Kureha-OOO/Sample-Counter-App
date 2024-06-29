ee542340c4158d53e558a16f97ea5833f0b3021d
Kureha_OOO <fuu34875tomo@gmail.com>
2024-06-29 15:43:50 +0900

    fix comments to Japanese

--- src/components/molecules/Buttons/index.stories.ts
+++ src/components/molecules/Buttons/index.stories.ts
@@ -1,25 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Buttons } from './index';
 
-
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: 'Counter/Molecules/Button',
   component: Buttons,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
 } satisfies Meta<typeof Buttons>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const CountButtons: Story = {
   args: {},
 };
